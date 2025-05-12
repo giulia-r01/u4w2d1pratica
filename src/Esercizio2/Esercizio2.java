0854e7357ad6a2ca569789c564a706cfe405917d
Giulia Rizzo <girzzo@gmail.com>
2025-05-12 16:44:28 +0200

    Update Esercizio2.java

--- src/Esercizio2/Esercizio2.java
+++ src/Esercizio2/Esercizio2.java
@@ -6,22 +6,25 @@ public class Esercizio2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        try {
-//            System.out.println("Inserisci i km percorsi (cifra intera): ");
-//            int km = scanner.nextInt();
-//
-//            System.out.println("Inserisci il numero intero dei litri di carburante consumati: ");
-//            int lt = scanner.nextInt();
-//
-//            int consumo = km/lt;
-//            System.out.println("Consumo (km/litro) = " + consumo);
-//        }
-//        catch (ArithmeticException e){
-//            System.out.println("Errore: non puoi dividere per 0. I litri devono essere > di 0");
-//        }
-//        catch (Exception e){
-//            System.out.println("Errore: Assicurati di inserire numeri interi");
-//        }
+        try {
+            System.out.println("Inserisci i km percorsi (cifra intera): ");
+            int km = scanner.nextInt();
+
+            System.out.println("Inserisci il numero intero dei litri di carburante consumati: ");
+            int lt = scanner.nextInt();
+
+            int consumo = km/lt;
+            System.out.println("Consumo (km/litro) = " + consumo);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Errore: non puoi dividere per 0. I litri devono essere > di 0");
+        }
+        catch (Exception e){
+            System.out.println("Errore: Assicurati di inserire numeri interi");
+        }
+
+//        In Java, la divisione int / 0 lancia un'eccezione ArithmeticException automaticamente.
+//        Ma con i double, NON viene generata eccezione, anche se dividi per zero.
 
         try {
             System.out.println("Inserisci i km percorsi (cifra decimale): ");
