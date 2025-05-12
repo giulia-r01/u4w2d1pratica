9b5f9bfc3bd29b4bf14978e964c38ec4d3b95838
Giulia Rizzo <girzzo@gmail.com>
2025-05-12 17:32:20 +0200

    Update Esercizio1.java

--- src/esercizio1/Esercizio1.java
+++ src/esercizio1/Esercizio1.java
@@ -21,37 +21,37 @@ public class Esercizio1 {
         System.out.println();
 
         while (true){
-        try {
-            System.out.println("Inserisci la posizione (da 0 a 4) da modificare: (digita 5 per terminare)");
-            int posizione = scanner.nextInt();
+            try {
+                System.out.println("Inserisci la posizione (da 0 a 4) da modificare: (digita 5 per terminare)");
+                int posizione = scanner.nextInt();
 
-            if (posizione == 5) {
-                System.out.println("Programma terminato");
-                break;
+                if (posizione == 5) {
+                    System.out.println("Programma terminato");
+                    break;
+                }
+
+                System.out.println("Inserisci il nuovo valore (digita 0 per terminare): ");
+                int nuovoValore = scanner.nextInt();
+
+                if (nuovoValore == 0) {
+                    System.out.println("Programma terminato");
+                    break;
+                }
+                numeri[posizione] = nuovoValore;
+
+                System.out.println("Array aggiornato");
+                for (int i = 0; i < numeri.length; i++) {
+                    System.out.println(numeri[i] + " ");
+                }
+                System.out.println();
+
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Errore: hai scelto una posizione che va oltre il limite (da 0 a 4)");
             }
-
-            System.out.println("Inserisci il nuovo valore (digita 0 per terminare): ");
-            int nuovoValore = scanner.nextInt();
-
-            if (nuovoValore == 0) {
-                System.out.println("Programma terminato");
-                break;
+            catch (Exception e){
+                System.out.println("Errore di input. Assicurati di inserire numeri interi");
+                scanner.nextLine();
             }
-            numeri[posizione] = nuovoValore;
-
-            System.out.println("Array aggiornato");
-            for (int i = 0; i < numeri.length; i++) {
-                System.out.println(numeri[i] + " ");
-            }
-            System.out.println();
-
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Errore: hai scelto una posizione che va oltre il limite (da 0 a 4)");
-        }
-        catch (Exception e){
-            System.out.println("Errore di input. Assicurati di inserire numeri interi");
-            scanner.nextLine();
-        }
 
 
         }
